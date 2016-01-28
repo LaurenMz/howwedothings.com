b4a38f82f0f2b8a7dd84855bb8a26ccb4d273fa7
Leo Bauza <leob@njimedia.com>
2016-01-28 15:21:22 -0500

    task(gulp) new config.json

--- gulpfile.js/tasks/sprite/sprite.js
+++ gulpfile.js/tasks/sprite/sprite.js
@@ -1,7 +1,19 @@
-var gulp = require('gulp'),
+var config = require('../../config');
+
+if (!config.tasks.sprite) { return; }
+
+var gulp =      require('gulp'),
     svgSprite = require('gulp-svg-sprite'),
-    svg2png = require('gulp-svg2png'),
-    config = require('../../config').sprite;
+    svg2png =   require('gulp-svg2png'),
+    path =      require('path');
+
+var paths: {
+  src: path.join(config.root.src, config.tasks.sprite.entry),
+  dest: config.root.base,
+  scssDest: path.join(config.root.src, config.tasks.sprite.scssDest),
+  spriteDest: path.join(config.root.dest, config.tasks.sprite.spriteDest),
+  spriteEndLocation: path.join(config.root.base, config.root.dest, config.tasks.sprite.spriteDest)
+};
 
 gulp.task('svgSprite', function () {
 
@@ -19,14 +31,14 @@ gulp.task('svgSprite', function () {
         render: {
           scss: {
             template: './gulp/tasks/sprite/sprite-template.scss',
-            dest: config.scss
+            dest: paths.scssDest
           }
         },
-        sprite: config.sprite + config.name,
+        sprite: paths.spriteDest + config.tasks.sprite.fileName,
       }
     },
     variables: {
-      spriteFile: config.name,
+      spriteFile: config.tasks.sprite.fileName,
       calcOffset: function () {
         return function (values, render) {
           var vals = render(values).split('|');
@@ -41,16 +53,16 @@ gulp.task('svgSprite', function () {
     }
   };
 
-  return gulp.src(config.entry)
+  return gulp.src(paths.src)
     .pipe(svgSprite(sprite))
-    .pipe(gulp.dest(config.dest));
+    .pipe(gulp.dest(paths.dest));
 
 });
 
 gulp.task('pngSprite', ['svgSprite'], function() {
-  return gulp.src(config.img + '/' + config.name)
+  return gulp.src(paths.spriteEndLocation + '/' + config.tasks.sprite.fileName)
     .pipe(svg2png())
-    .pipe(gulp.dest(config.img));
+    .pipe(gulp.dest(paths.spriteEndLocation));
 });
 
 gulp.task('sprite', ['pngSprite']);
